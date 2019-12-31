f9a59225fc1fe45910dba64d5b1580098070782c
RajeshRam78 <48416054+RajeshRam78@users.noreply.github.com>
2019-12-31 18:15:18 +0530

    project files

--- main1.py
+++ main1.py
@@ -13,12 +13,11 @@ try:
     print("found")
 except:
     print("Folder not found")
-    time.sleep(1)
+    time.sleep(5)
     exit()
 
-# class to store the file parameters
 class File_tp(object):
-    def __init__(self, file_name, file_path):
+    def __init__(self, file_name, file_path): #, file_size):
         self.file_name = file_name
         self.file_path_list = [file_path]
         self.file_size = os.path.getsize(file_path + "\\" +file_name)
@@ -26,16 +25,12 @@ class File_tp(object):
 
 
 File_object_list = []
-Duplicate_count = 0
+
 def find_duplicate_files(Root_path):
-    global  Duplicate_count
-    # Make file hash table to store the files based on the file's first letter
+
+    # Make file hash table
     File_hash_table = {}
 
-    # Make file hash table to store the file objects based on the file's first letter
-    File_objects_hash = {}
-
-    # Create file hash table
     for folder_path, folder_dir, file_names in os.walk(Root_path):
         for file_name in file_names:
             try:
@@ -43,39 +38,40 @@ def find_duplicate_files(Root_path):
             except:
                 File_hash_table[file_name[0]] = [(file_name, folder_path)]
 
-    # list to store the file objects with same starting letter
+    Duplicate_objects = []
     File_object_list = []
-
-    # Each hash element in the file hash table
+    duplicate_count = 0
     for hash_element in File_hash_table:
-        # Iterate through each element of hash array
         for file_name, folder_path in File_hash_table[hash_element]:
-            duplicate = False
+            file_isduplicate = False
             for File_object in File_object_list:
-                if file_name == File_object.file_name: # compare if the file name exist already
-                    duplicate = (os.path.getsize(folder_path + "\\" + file_name) == File_object.file_size)
-                if duplicate:
+                name_equal = (file_name == File_object.file_name)
+                size_equal = (os.path.getsize(folder_path + "\\" + file_name) == File_object.file_size)
+                if name_equal and size_equal:
                     File_object.file_count += 1
                     File_object.file_path_list.append(folder_path)
+                    file_isduplicate = True
                     print(File_object.file_count,File_object.file_name)
-                    Duplicate_count += 1
+                    duplicate_count += 1
                     break
                 else:
                     pass
-            if not duplicate:
+            if not file_isduplicate:
                 File_object = File_tp(file_name, folder_path)
                 File_object_list.append(File_object)
 
+        Duplicate_objects.append(File_object_list)
+        File_object_list = []
 
-        File_objects_hash[file_name[0]] = File_object_list
-
-
-if __name__ == "__main__":
-    init_time = time.time()
-    find_duplicate_files(Root_path)
-    print(time.time() - init_time)
-
-    print("Total duplicate files = {}".format(Duplicate_count))
+    print(duplicate_count)
 
 
+
+
+
+
+
+init_time = time.time()
+find_duplicate_files(Root_path)
+print(time.time()-init_time)
 
